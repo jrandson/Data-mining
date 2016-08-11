fe82e27cd712e05b8dc7b8e52418724328393324
Randson <jrandson>
2016-08-11 11:30:17 -0300

    changing scale

--- app/ch4.py
+++ app/ch4.py
@@ -32,10 +32,13 @@ class Classifier():
       f = open(filename)
       lines = f.readlines()
       f.close()
+
       self.format = lines[0].strip().split('\t')
+
       self.data = []
       for line in lines[1:]:
          fields = line.strip().split('\t')
+
          ignore = []
          vector = []
          for i in range(len(fields)):
@@ -46,13 +49,20 @@ class Classifier():
             elif self.format[i] == 'class':
               classification = fields[i]
          self.data.append((classification, vector, ignore))
+         
+
       self.rawData = list(self.data)
+
       # get length of instance vector
       self.vlen = len(self.data[0][1])
+
+
       # now normalize the data
       for i in range(self.vlen):
          self.normalizeColumn(i)
-   
+
+      self.medianAndDeviation = self.getMedianAndDeviation()
+
 
    def euclidian(self,rate1, rate2):
       distcance = 0
@@ -160,7 +170,7 @@ class Classifier():
 
       return soma/float(len(values))
 
-   def medianAndDeviation(self):
+   def getMedianAndDeviation(self):
 
       median = self.getMedian(self.data[1][1])
       deviation = self.asd(self.data[1][1])
@@ -203,24 +213,31 @@ class Classifier():
 
       return data
 
-   def normalizeColumn(self,colum_Number):
-      col = [v[1][colum_Number] for v in self.data]
+   def normalizeColumn(self,columnNumber):
 
+      """given a column number, normalize that column in self.data"""
+      # first extract values to list
+      col = [v[1][columnNumber] for v in self.data]
       median = self.getMedian(col)
       asd = self.asd(col)
-
+      #print("Median: %f   ASD = %f" % (median, asd))
+      self.medianAndDeviation.append((median, asd))
       for v in self.data:
-         v[1][colum_Number] = (v[1][colum_Number] - median)/asd
+         v[1][columnNumber] = (v[1][columnNumber] - median) / asd
+
 
    def normalizeVector(self, v):
       """We have stored the median and asd for each column.
-      We now use them to normalize vector v"""
+      We now use them to normalize vector v"""  
+
       vector = list(v)
+
       for i in range(len(vector)):
-         (median, asd) = self.medianAndDeviation[i]
+         (median, asd) = self.getMedianAndDeviation()
          vector[i] = (vector[i] - median) / asd
+
       return vector
-        
+
    def manhattan(self, vector1, vector2):
       """Computes the Manhattan distance."""
       return sum(map(lambda v1, v2: abs(v1 - v2), vector1, vector2))
@@ -266,33 +283,57 @@ def unitTest():
     nl = ('Gymnastics', [61, 76], ['Viktoria Komova'])
     cl = ("Basketball", [74, 190], ['Crystal Langhorne'])
     # first check normalize function
+
     brNorm = classifier.normalizeVector(br[1])
     nlNorm = classifier.normalizeVector(nl[1])
     clNorm = classifier.normalizeVector(cl[1])
+
+    
+
     assert(brNorm == classifier.data[1][1])
     assert(nlNorm == classifier.data[-1][1])
     print('normalizeVector fn OK')
+    
     # check distance
     assert (round(classifier.manhattan(clNorm, classifier.data[1][1]), 5) == 1.16823)
     assert(classifier.manhattan(brNorm, classifier.data[1][1]) == 0)
     assert(classifier.manhattan(nlNorm, classifier.data[-1][1]) == 0)
     print('Manhattan distance fn OK')
+    
     # Brittainey Raven's nearest neighbor should be herself
     result = classifier.nearestNeighbor(brNorm)
     assert(result[1][2]== br[2])
+    
     # Nastia Liukin's nearest neighbor should be herself
     result = classifier.nearestNeighbor(nlNorm)
     assert(result[1][2]== nl[2])
+    
     # Crystal Langhorne's nearest neighbor is Jennifer Lacy"
     assert(classifier.nearestNeighbor(clNorm)[1][2][0] == "Jennifer Lacy")
     print("Nearest Neighbor fn OK")
+    
     # Check if classify correctly identifies sports
     assert(classifier.classify(br[1]) == 'Basketball')
     assert(classifier.classify(cl[1]) == 'Basketball')
     assert(classifier.classify(nl[1]) == 'Gymnastics')
     print('Classify fn OK')
 
-unitTest()
+#unitTest()
+
+values = [10,23,45,56,57,103]
+
+def change_scale(values,minimo = 0,maximo = 1):
+
+   new_scale = [ minimo + (maximo - minimo)*i/float(max(values)) for i in values]
+   print new_scale
+   print ''
+   scale2 = [ minimo + (maximo-minimo)*(i - min(values))/float(max(values) - min(values)) for i in values]
+   print scale2
+   print ''
+
+change_scale(values,-1,1)
+
+
     
 
 
